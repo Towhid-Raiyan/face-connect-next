db981a3cad04086cd90f3dcdf91a526b00844b49
Towhid-Raiyan <towhid.raiyan.com>
2023-09-08 15:55:23 +0600

    bug fixed for deploy

--- src/app/Room/page.jsx
+++ src/app/Room/page.jsx
@@ -76,21 +76,90 @@
 //     ></div>
 //   );
 // }
-import { useEffect, useRef } from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function randomID(len) {
-  let result = '';
-  if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
+//------------------------------------------------------------------------------------------------------
+// import { useEffect, useRef } from 'react';
+// import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+// function randomID(len) {
+//   let result = '';
+//   if (result) return result;
+//   var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+//     maxPos = chars.length,
+//     i;
+//   len = len || 5;
+//   for (i = 0; i < len; i++) {
+//     result += chars.charAt(Math.floor(Math.random() * maxPos));
+//   }
+//   return result;
+// }
+
+// export function getUrlParams(
+//   url = typeof window !== 'undefined' ? window.location.href : ''
+// ) {
+//   let urlStr = url.split('?')[1];
+//   return new URLSearchParams(urlStr);
+// }
+
+// export default function VideoCall() {
+//   const roomID = getUrlParams().get('roomID') || randomID(5);
+//   const containerRef = useRef(null);
+
+//   useEffect(() => {
+//     async function startVideoCall() {
+//       if (typeof window !== 'undefined') { // Check if running in a browser environment
+//         // Generate Kit Token
+//         const appID = 658986879;
+//         const serverSecret = "84b1cb33a6b72b91bc1a7a42d5b2013f";
+//         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+//           appID,
+//           serverSecret,
+//           roomID,
+//           randomID(5),
+//           randomID(5)
+//         );
+
+//         // Create instance object from Kit Token
+//         const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+//         // Start the call
+//         zp.joinRoom({
+//           container: containerRef.current,
+//           sharedLinks: [
+//             {
+//               name: 'Personal link',
+//               url:
+//                 window.location.protocol +
+//                 '//' +
+//                 window.location.host +
+//                 window.location.pathname +
+//                 '?roomID=' +
+//                 roomID,
+//             },
+//           ],
+//           scenario: {
+//             mode: ZegoUIKitPrebuilt.GroupCall,
+//           },
+//         });
+//       }
+//     }
+
+//     startVideoCall();
+//   }, [roomID]);
+
+//   return (
+//     <div
+//       className="myCallContainer"
+//       ref={containerRef}
+//       style={{ width: '100vw', height: '100vh' }}
+//     ></div>
+//   );
+// }
+
+//----------------------------------------------------------------------------
+
+import { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
 
 export function getUrlParams(
   url = typeof window !== 'undefined' ? window.location.href : ''
@@ -99,16 +168,38 @@ export function getUrlParams(
   return new URLSearchParams(urlStr);
 }
 
+// Define the randomID function
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars =
+    '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP';
+  var maxPos = chars.length;
+  var i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
 export default function VideoCall() {
   const roomID = getUrlParams().get('roomID') || randomID(5);
   const containerRef = useRef(null);
 
   useEffect(() => {
     async function startVideoCall() {
-      if (typeof window !== 'undefined') { // Check if running in a browser environment
-        // Generate Kit Token
+      if (process.browser) {
+        // Check if running in the browser environment
+
+        // Load ZegoUIKitPrebuilt dynamically
+        const { ZegoUIKitPrebuilt } = await import('@zegocloud/zego-uikit-prebuilt');
+
+        // Replace these with your actual appID and serverSecret
         const appID = 658986879;
-        const serverSecret = "84b1cb33a6b72b91bc1a7a42d5b2013f";
+        const serverSecret = '84b1cb33a6b72b91bc1a7a42d5b2013f';
+
+        // Generate the Zego token
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
           appID,
           serverSecret,
@@ -120,7 +211,6 @@ export default function VideoCall() {
         // Create instance object from Kit Token
         const zp = ZegoUIKitPrebuilt.create(kitToken);
 
-        // Start the call
         zp.joinRoom({
           container: containerRef.current,
           sharedLinks: [
@@ -153,4 +243,7 @@ export default function VideoCall() {
     ></div>
   );
 }
+
+
+
 
